f44c0dbd18c15d7d4b0f3a4e920695da97fd73d4
KimSeongHeon <47879772+KimSeongHeon@users.noreply.github.com>
2023-03-23 12:40:39 +0900

    [buildSrc] buildSrc dependency 추가
    - network 사용을 위한 ktor 추가

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -9,6 +9,20 @@ object Dependencies {
             const val TEST = "io.insert-koin:koin-test:${Versions.KOIN_VERSION}"
             const val ANDROID = "io.insert-koin:koin-android:${Versions.KOIN_VERSION}"
         }
+    }
 
+    object Network {
+        object Ktor {
+            const val CORE = "io.ktor:ktor-client-core:${Versions.KTOR_VESRION}"
+            const val SERIALIZATION = "io.ktor:ktor-client-serialization:${Versions.KTOR_VESRION}"
+
+            object Android {
+                const val CLIENT = "io.ktor:ktor-client-android:${Versions.KTOR_VESRION}"
+            }
+
+            object iOS {
+                const val CLIENT = "io.ktor:ktor-client-ios:${Versions.KTOR_VESRION}"
+            }
+        }
     }
 }
--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -1,4 +1,5 @@
 object Versions {
     const val COROUTINE_VERSION = "1.6.4"
     const val KOIN_VERSION = "3.2.0"
+    const val KTOR_VESRION = "2.2.4"
 }
