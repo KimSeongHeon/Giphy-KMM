fb627a82df62dad21265e7a4159680d23dfb37b8
KimSeongHeon <47879772+KimSeongHeon@users.noreply.github.com>
2023-04-11 22:29:20 +0900

    [macOS] 로그 수정

--- shared/src/jvmMain/kotlin/com/example/giphy_kmm/utils/Logger.kt
+++ shared/src/jvmMain/kotlin/com/example/giphy_kmm/utils/Logger.kt
@@ -1,5 +1,5 @@
 package com.example.giphy_kmm.utils
 
-actual fun debugLog(tag: String, log: String) = Unit
+actual fun debugLog(tag: String, log: String) = println("tag $log")
 
-actual fun infoLog(tag: String, log: String) = Unit
+actual fun infoLog(tag: String, log: String) = println("tag $log")
